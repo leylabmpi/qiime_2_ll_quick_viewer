5340309502b488ec812346f64fc008e54e5012e9
Guillermo Luque <gluque@a3-luque-mbp.eb.local>
2017-11-27 16:27:06 +0100

    Enabling server

--- qiime_2_ll_quick_viewer/cli.py
+++ qiime_2_ll_quick_viewer/cli.py
@@ -3,14 +3,17 @@
 """Console script for qiime_2_ll_quick_viewer."""
 
 import click
-
+from qiime_2_ll_quick_viewer import launch_server
 
 @click.command()
-def main(args=None):
-    """Console script for qiime_2_ll_quick_viewer."""
-    click.echo("Replace this message by putting your code into "
-               "qiime_2_ll_quick_viewer.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+@click.option('-f', '--filename', required=True, help="Full path to a Qiime2 visualization file (*.qzv).")
+@click.option('-p', '--port', required=True, default=8080, help="Port to launch the web server.")
+def main(filename, port):
+    """Quick viewer for Qiime 2 visualization artifacts.\n
+    It launches a simple webserver to visualize the contents of the data folder in a browser.
+    The webserver is launched in your server and you can access through a SSH session with X11 port forwarding enabled.
+    """
+    launch_server(filename, port)
 
 
 if __name__ == "__main__":
--- qiime_2_ll_quick_viewer/qiime_2_ll_quick_viewer.py
+++ qiime_2_ll_quick_viewer/qiime_2_ll_quick_viewer.py
@@ -1,3 +1,28 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+import http.server
+import socketserver
+import os
+import sys
+import zipfile
+import secrets
+import tempfile
+import click
+
+
+def launch_server(filename, port):
+    tmp_dir = tempfile.gettempdir()
+    tmp_dir = os.path.join(tmp_dir, secrets.token_hex(8))
+
+    with zipfile.ZipFile(filename, 'r') as zip_ref:
+        contents = zip_ref.namelist()
+        index_html = [x for x in contents if x.endswith('index.html')][0]
+        web_dir = os.path.join(tmp_dir, os.path.dirname(index_html))
+        zip_ref.extractall(tmp_dir)
+
+    os.chdir(web_dir)
+    handler = http.server.SimpleHTTPRequestHandler
+    httpd = socketserver.TCPServer(("", port), handler)
+    click.echo("Qiime 2 Ley Lab Viewer serving at port %s, from folder %s" % (port, web_dir))
+    httpd.serve_forever()
